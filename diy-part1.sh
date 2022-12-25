3acb68d1d728162aa38ac235dad9eb235a4b9c21
AXBNG <89022914+AXBNG@users.noreply.github.com>
2022-12-25 17:15:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,4 +11,5 @@
 #
 
 # Add a feed source
-echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
