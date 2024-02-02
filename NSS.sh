f233d67b953c9f99a6d249aa0eac6ce16f105868
AXBNG <89022914+AXBNG@users.noreply.github.com>
2024-02-02 12:58:40 +0800

    Update NSS.sh

--- NSS.sh
+++ NSS.sh
@@ -14,11 +14,14 @@
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 
 # 修正
-rm -rf feeds/luci/modules/luci-base && git clone https://github.com/immortalwrt/luci/tree/master/modules/luci-base feeds/luci/modules/luci-base
-rm -rf feeds/luci/modules/luci-mod-status && git clone https://github.com/immortalwrt/luci/tree/master/modules/luci-mod-status feeds/luci/modules/luci-mod-status
+rm -rf feeds/luci/modules/luci-base
+rm -rf feeds/luci/modules/luci-mod-status
 rm -rf feeds/packages/utils/coremark
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/nss-packages/utils/mhz
+
+svn export https://github.com/immortalwrt/luci/tree/master/modules/luci-base feeds/luci/modules/luci-base
+svn export https://github.com/immortalwrt/luci/tree/master/modules/luci-mod-status feeds/luci/modules/luci-mod-status
 
 echo 'refresh feeds'
 ./scripts/feeds update -a
